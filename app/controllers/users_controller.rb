6a7d45aa6ca4713499e8e4a301de2a8b196fe468
Stefano Chiesa <base278@gmail.com>
2015-07-13 05:27:45 -0500

    Add view all users page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,11 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:index, :edit, :update]
+  before_action :correct_user, only: [:edit, :update]
+
+  def index
+    @users = User.paginate(page: params[:page])
+  end
+
   def new
     @user = User.new
   end
@@ -31,6 +38,25 @@ class UsersController < ApplicationController
       render 'edit'
     end
   end
+
+  def logged_in_user
+    unless logged_in?
+      store_location
+      flash[:danger] = "Please log in."
+      redirect_to login_url
+    end
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless correct_user?(@user)
+  end
+
+  def correct_user?(user)
+    user == current_user
+  end
+
+  
 
   private
 
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -38,4 +38,13 @@ module SessionsHelper
     @current_user = nil
   end
 
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+
+  def store_location
+    session[:forwarding_url] = request.url if request.get?
+  end
+
 end

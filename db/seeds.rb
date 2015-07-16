0d359eb025705f22fed984e62d9ba78b6c1308e2
Stefano Chiesa <base278@gmail.com>
2015-07-15 21:59:24 -0500

    Add view for micropost in user profile

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,7 +10,7 @@ User.create!(name: "Example User",
              password: "foobar",
              password_confirmation: "foobar",
              admin: true,
-             activated: false,
+             activated: true,
              activated_at: Time.zone.now())
 
 99.times do |n|
@@ -23,4 +23,10 @@ User.create!(name: "Example User",
                password_confirmation: password,
                activated: true,
                activated_at: Time.zone.now())
+end
+
+users = User.order(:created_at).take(6)
+50.times do
+  content = Faker::Lorem.sentence(5)
+  users.each { |user| user.microposts.create!(content: content) }
 end
